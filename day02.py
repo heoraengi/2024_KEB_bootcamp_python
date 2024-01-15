1407333b2476e9b4a83ea649239e4df3962554d8
heoraengi <heo.raeng.i@gmail.com>
2024-01-15 11:43:32 +0900

    v0.5 python day02

--- day02.py
+++ day02.py
@@ -18,7 +18,28 @@ subjects[3] = 'f'
 print(subjects[3]) # 출력 : f # mutable이라서 값 변경 가능
 '''
 
-### 3장 숫자 ###
 # literal 정수는 변수가 아닌 값 그 자체를 말함; 데이터 그 자체임
 # python console에 help("keywords") 실행하면 키워드가 나옴
-# 변수명 생성 시 키워드 불가, 숫자로 시작 불가ddl
+# 변수명 생성 시 키워드 불가, 숫자로 시작 불가, 특수기호는 _만 가능
+
+### 할당 ###
+'''
+x = 2
+y = x + 5
+print(y)
+
+print(type(3.14))
+print(type(3.14) == float) # True
+print(isinstance(3.14, float)) # True
+print(isinstance("inha", float)) # False
+print(isinstance(55, float)) # False
+'''
+### 복사 ###
+artists = ['BTS', 'NewJeans', 'RISE', 'SES']
+groups = artists
+print(artists, groups)
+# ['BTS', 'NewJeans', 'RISE', 'SES'] ['BTS', 'NewJeans', 'RISE', 'SES']
+artists[3] = 'aespa'
+print(artists, groups)
+# ['BTS', 'NewJeans', 'RISE', 'aespa'] ['BTS', 'NewJeans', 'RISE', 'aespa']
+# groups 변수는 artists 변수를 참조했으므로 서로 같은 주소를 바라보고 있어 artists가 바끼면 groups도 바뀐다.
