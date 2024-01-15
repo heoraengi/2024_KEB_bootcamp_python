aa3ad5ea6a33a718cb9a3050170fc3c1ecb50518
heoraengi <heo.raeng.i@gmail.com>
2024-01-15 13:21:39 +0900

    v0.7 python day02

--- day02.py
+++ day02.py
@@ -54,7 +54,16 @@ cash = 3_000_000
 print(cash) # 3000000
 print(type(cash)) # <class 'int'>
 
-### 입력 및 연산 ###
+### 입력 및 연산 및 출력 ###
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input exponent number : '))
+# f-string
 print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+
+# format function
+print('밑은 {0}, 지수는 {1} 결과 값은 {2}'.format(base_number,exponent_number,pow(base_number, exponent_number)))
+print('밑은 {}, 지수는 {} 결과 값은 {}'.format(base_number,exponent_number,pow(base_number, exponent_number))) # 순서대로 입력하는 거면 숫자 안넣어도 됨
+
+# c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number,exponent_number,pow(base_number, exponent_number)))
