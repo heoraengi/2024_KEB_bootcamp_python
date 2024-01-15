79284675bea78429529d76178dd58d57c50809f2
heoraengi <heo.raeng.i@gmail.com>
2024-01-15 10:57:07 +0900

    v0.4 python day02

--- day02.py
+++ day02.py
@@ -1,3 +1,5 @@
+### 2장 데이터 ###
+'''
 print(0.1, 1e-1)
 print(0.01, 1e-2)
 print(314.865, 0.341865e3) # e 지수 표현
@@ -14,4 +16,9 @@ print(subjects[3]) # 출력 : d
 print(subjects[3:4]) # 출력 : ['d']
 subjects[3] = 'f'
 print(subjects[3]) # 출력 : f # mutable이라서 값 변경 가능
+'''
 
+### 3장 숫자 ###
+# literal 정수는 변수가 아닌 값 그 자체를 말함; 데이터 그 자체임
+# python console에 help("keywords") 실행하면 키워드가 나옴
+# 변수명 생성 시 키워드 불가, 숫자로 시작 불가ddl
