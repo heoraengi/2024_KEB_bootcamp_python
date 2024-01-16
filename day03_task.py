b18942cb1eb1b0fc56a3e012b7c868960513f5ab
heoraengi <heo.raeng.i@gmail.com>
2024-01-16 16:08:41 +0900

    v0.1 day03_progject1

---
+++ day03_task.py
@@ -0,0 +1,56 @@
+##### 과제 1 #####
+
+while True :
+    menu = input("1) Fahrenheit -> Celsius  2) Fahrenheit -> Celsius 3) Prime number 4) Range Prime number  5) Quit program : ")
+
+    if menu =='1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(
+            f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
+        # .2f로 소수점 2번쨰자리까지 출력 (f스트링 서식)
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        is_prime = True
+
+        if number < 2:
+            print(f'{number} is not prime number')
+        else:
+            for i in range(2, number):
+                if number % i == 0:
+                    is_prime = False
+                    break
+
+            if is_prime:
+                print(f'{number} is prime number')
+            else:
+                print(f'{number} is not prime number')
+    elif menu == '4':
+        numbers = input("Input First Second number : ").split()
+        n1, n2 = int(numbers[0]), int(numbers[1])
+        if n1 > n2:
+            n1, n2 = n2, n1
+        # numbers = [int(n) for n in numbers]
+        # n1, n2 = min(numbers), max(numbers)
+
+        is_prime = True
+        for number in range(n1, n2 + 1):
+            is_prime = True
+
+            if number < 2:
+                pass
+            else:
+                for i in range(2, number):
+                    if number % i == 0:
+                        is_prime = False
+                        break
+                if is_prime:
+                    print(number, end=' ')
+        print()
+    elif menu == '5':
+        print("Terminate program")
+        break
+    else:
+        print("choose number 1 ~ 5!")
