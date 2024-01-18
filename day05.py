cdc94ea37eb7323335d932c04959a88d06a99f0e
heoraengi <heo.raeng.i@gmail.com>
2024-01-18 14:24:29 +0900

    v0.48 day05

--- day05.py
+++ day05.py
@@ -179,3 +179,59 @@ print(type(x)) # <class 'function'>
 print(x())# 25 <- 제대로 출력하고 싶으면 ()를 넣어줘야함
 '''
 
+### map 사용법 ###
+numbers = ["7",'1','2']
+m = sum(map(int,numbers))
+print(m)
+
+def is_prime(n) -> bool :
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean type으로 return
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n :
+            if n % i == 0:
+                return False
+            i += 1
+    return True
+
+while True :
+    menu = input("1) Fahrenheit -> Celsius  2) Fahrenheit -> Celsius 3) Prime number 4) Range Prime number  5) Quit program : ")
+
+    if menu =='1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(
+            f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
+        # .2f로 소수점 2번쨰자리까지 출력 (f스트링 서식)
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+
+        if is_prime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is not prime number')
+    elif menu == '4':
+        n1, n2 = map(int, input("Input First Second number : ").split())
+        # numbers = input("Input First Second number : ").split()
+        # n1, n2 = int(numbers[0]), int(numbers[1])
+        n1, n2 =min(n1,n2) , max(n1,n2)
+        # if n1 > n2:
+        #     n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            if is_prime(number):
+                print(number, end=' ')
+        print()
+    elif menu == '5':
+        print("Terminate program")
+        break
+    else:
+        print("choose number 1 ~ 5!")
