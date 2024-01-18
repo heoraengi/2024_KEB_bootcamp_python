a87d9ffdeea47a689c67e6a258ccbe60e8f2bb79
heoraengi <heo.raeng.i@gmail.com>
2024-01-18 10:52:16 +0900

    v0.44 prime number function  day05

--- day05.py
+++ day05.py
@@ -1,4 +1,5 @@
 ### dict comprehension
+'''
 univ = 'inha university'
 counts_alphabet = {letter:univ.count(letter) for letter in univ}
 print(counts_alphabet)
@@ -14,3 +15,33 @@ print(counts_alphabet)
 #8-10
 squares = {i:i*i for i in range(10)}
 print(squares) # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
+'''
+
+##### 9장 함수 ######
+### 구간 소수 판정 프로그램 함수화###
+def is_prime(n) -> bool :
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean type으로 return
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n :
+            if n % i == 0:
+                return False
+            i += 1
+    return True
+
+# help(is_prime) # 함수에 대한 설명 출력
+
+numbers = input("Input First Second number : ").split()
+n1, n2 = int(numbers[0]), int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2 + 1):
+    if is_prime(number):
+        print(number, end=' ')
