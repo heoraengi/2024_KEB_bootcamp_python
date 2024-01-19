4de7987652092f99e1d5716c6ac40340c5821e9f
heoraengi <heo.raeng.i@gmail.com>
2024-01-19 15:10:04 +0900

    v0.62 day06

--- day06.py
+++ day06.py
@@ -192,6 +192,7 @@ p3= Pokemon("아무개")
 p1.attack(p2) # 피카츄이(가) 꼬부기을(를) 전기 공격!
 print(p1.name,p1.type)
 '''
+'''
 class Animal:
     def says(self):
         return 'I speak'
@@ -220,3 +221,27 @@ print(h1.says()) # 버새 버새~
 print(Hinny.__mro__) # 족보의 우선순위를 보여줌
 # (<class '__main__.Hinny'>, <class '__main__.Horse'>, <class '__main__.Donkey'>, <class '__main__.Animal'>, <class 'object'>)
 # 자기자신 - 첫번째 상속 - 두번째 상속 - 상속받은 클래스의 부모 클래스
+'''
+
+class FlyingMixin:
+    def fly(self):
+        return f'{self.name}이(가) 날다~'
+
+class SwimmingMixin:
+    def swim(self):
+        return f'{self.name}이(가) 수영을 합니다~'
+
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+
+class Charizard(Pokemon, FlyingMixin):
+    pass
+
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+
+g1 = Gyarados('갸라도스')
+c1 = Charizard('리자몽')
+print(g1.swim()) # 갸라도스이(가) 수영을 합니다~
+print(c1.fly()) # 리자몽이(가) 날다~
