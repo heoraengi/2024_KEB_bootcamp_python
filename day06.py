6ee2c9f720baf565aed828f0c70e90d30ba7835e
heoraengi <heo.raeng.i@gmail.com>
2024-01-19 09:17:53 +0900

    v0.53 assignment 9.2 day06

--- day06.py
+++ day06.py
@@ -9,4 +9,20 @@ def good():
 
 print(good())
 '''
+def get_odds(n) -> int:
+    """
+    1~n까지의 홀수를 발생시키는 제너레이터
+    :param n: int
+    :return: int
+    """
+    for i in range(1, n+1, 2):
+        yield i
+
+cnt = 0
+odds = get_odds(9)
+for odd in odds:
+    cnt+=1
+    if cnt == 3:
+        print(f'Third number is {odd}') # Third number is 5
+        break
 
