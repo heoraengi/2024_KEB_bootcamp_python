e0b98331293a1d0ea284ba7f5f1676fbc0596485
heoraengi <heo.raeng.i@gmail.com>
2024-01-19 09:29:21 +0900

    v0.54 assignment 9.3(a) day06

--- day06.py
+++ day06.py
@@ -9,6 +9,7 @@ def good():
 
 print(good())
 '''
+'''
 def get_odds(n) -> int:
     """
     1~n까지의 홀수를 발생시키는 제너레이터
@@ -25,4 +26,25 @@ for odd in odds:
     if cnt == 3:
         print(f'Third number is {odd}') # Third number is 5
         break
+'''
+def test(f) :
+    """
+    데코레이터 함수 : 시작하면 start 출력, 끝나면 end 출력
+    :param f: function
+    :return: closer function
+    """
+    def test_in(*args, **kwargs):
+        print('start')
+        result = f(*args, **kwargs)
+        print('end')
+        return result
+    return test_in
+
+
+def greeting():
+    print("안녕하세요!!!")
+
+
+t =test(greeting)
+t()
 
