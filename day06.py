6e9096043d65b2546b9b0e752864dae49c4bb00f
heoraengi <heo.raeng.i@gmail.com>
2024-01-19 10:35:10 +0900

    v0.56 day06

--- day06.py
+++ day06.py
@@ -9,6 +9,7 @@ def good():
 
 print(good())
 '''
+
 '''
 def get_odds(n) -> int:
     """
@@ -26,6 +27,8 @@ for odd in odds:
     if cnt == 3:
         print(f'Third number is {odd}') # Third number is 5
         break
+'''
+
 '''
 def test(f) :
     """
@@ -46,4 +49,34 @@ def greeting():
     print("안녕하세요!!!")
 
 greeting()
+'''
 
+# 반복문 사용
+def factorial_repetition(n)-> int:
+    """
+    반복문으로 만든 팩토리얼 함수
+    :param n:
+    :return:
+    """
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
+
+def factorial_recursion(n):
+    """
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: int
+    :return: function, int
+    """
+    if n == 1 :
+        return n
+    else :
+        return n * factorial_recursion(n-1)
+
+number = int(input("number : "))
+print(factorial_repetition(number))
+print(factorial_recursion(number))
+print(globals())
+# 반복문은 속도가 빠름
+# 대신 재귀함수는 변수를 줄일 수 있음 :  side_effect을 막을 수 있음
